6e0ba48c2340d234ec6b2cebbd1e20dd35233b18
SimeonHristov99 <s.e.hristov99@gmail.com>
2021-10-28 12:00:06 +0300

    continue working on task 9

--- task9/task9.cpp
+++ task9/task9.cpp
@@ -1,56 +1,114 @@
 #include <iostream>
+#include <vector>
 #include <stack>
 
-std::string brainLuck(const std::string &code, const std::string &input) {
+std::string brainLuck(const std::string &code, const std::string &input)
+{
     int len_input = input.length();
-    
-    if (input.length() == 0) {
+    int len_code = code.length();
+
+    if (len_input == 0)
+    {
+        std::cout << "Input was empty!\n";
         return "";
     }
-    
+
+    if (len_code == 0)
+    {
+        std::cout << "Code was empty!\n";
+        return input;
+    }
+
+    // col1: indices of openning brackets
+    // col2: indices of closing brackets
+    // for ,[.[-],] this is [[1, 7], [3, 5]]
+    std::vector<std::vector<int>> brackets;
+
     std::string result_string;
     // result_string.resize(len_input, '\0');
 
-    std::stack<char> result_stack;
     std::string::const_iterator it_input = input.cbegin();
-    
-    std::stack<char> commands;
-    for (std::string::const_iterator it = code.cend(); it != code.cbegin() - 1; --it) {
-        commands.push(*it);
+
+    for (int i = 0; i < len_code; ++i)
+    {
+        if (code[i] == '[')
+        {
+            brackets.push_back({i, -1});
+        }
+        else if (code[i] == ']')
+        {
+            for (int j = brackets.size() - 1; j >= 0; --j)
+            {
+                if (brackets[j][1] == -1)
+                {
+                    brackets[j][1] = i;
+                    break;
+                }
+            }
+        }
     }
 
-    while (commands.top() != *(code.cend())) {
-        char op = commands.top();
-        commands.pop();
-
-        switch (op)
+    char data_pointer = '0';
+    for (int i = 0; i < len_code; ++i)
+    {
+        switch (code[i])
         {
-        case ',': {
-            result_stack.push(*it_input);
+        case ',':
+        {
+            data_pointer = *it_input;
+            ++it_input;
 
             break;
         }
-        case '.': {
-            result_string += result_stack.top();
-            result_stack.pop();
+        case '.':
+        {
+            result_string += data_pointer;
 
             break;
         }
-        case '+': {
-            char top = result_stack.top();
-            result_stack.push((top + 1) % 256);
+        case '+':
+        {
+            data_pointer = (data_pointer + 1) % 256;
 
             break;
         }
-        case '-': {
-            char top = result_stack.top();
-            std::cout << (int(top) - 1) << '\n';;
-            result_stack.push((unsigned((int)top - 1)) % 256);
+        case '-':
+        {
+            data_pointer = (char)(unsigned((int)data_pointer - 1)) % 256;
 
             break;
         }
-        default: {
-            std::cout << "ERROR: Unsupported symbol: " << op  << " (" << int(op) << ')' << '\n';
+        case '[':
+        {
+            if ((int)data_pointer == 0)
+            {
+                for (auto b : brackets) {
+                    if (b[0] == i) {
+                        i = b[1];
+                        break;
+                    }
+                }
+            }
+
+            break;
+        }
+        case ']':
+        {
+            if ((int)data_pointer != 0)
+            {
+                for (auto b : brackets) {
+                    if (b[1] == i) {
+                        i = b[0];
+                        break;
+                    }
+                }
+            }
+
+            break;
+        }
+        default:
+        {
+            std::cout << "ERROR: Unsupported symbol: " << code[i] << " (" << int(code[i]) << ')' << '\n';
             return "";
         }
         }
@@ -59,53 +117,46 @@ std::string brainLuck(const std::string &code, const std::string &input) {
     return result_string;
 }
 
-int main() {
-    // std::cout << ((unsigned(-1))%256) << '\n';
-
-    std::cout << ((char) 0) << '\n';
-
-    //echo until "0";
-    std::string tw = "";
-    tw.append(1,(char) 0);
-    std::cout << int(brainLuck(",-.",tw)[0]) << '\n'; // "c"
+int main()
+{
+    //echo until "255";
+    std::string tw = "c";
+    tw.append(1, (char)255);
+    std::cout << brainLuck(",.", tw) << '\n'; // "c"
 
     //echo until "255";
     tw = "c";
-    tw.append(1,(char) 255);
-    std::cout << brainLuck(",.",tw) << '\n'; // "c"
+    tw.append(1, (char)255);
+    std::cout << brainLuck(",-.", tw) << '\n'; // "c"
 
     //echo until "255";
     tw = "c";
-    tw.append(1,(char)255);
-    std::cout << brainLuck(",-.",tw) << '\n'; // "b"
-
-    //echo until "255";
-    tw = "c";
-    tw.append(1,(char)255);
-    std::cout << brainLuck(",+.",tw) << '\n'; // "d"
+    tw.append(1, (char)255);
+    std::cout << brainLuck(",+.", tw) << '\n'; // "d"
 
     //echo until "255";
     tw = "c0";
-    tw.append(1,(char) 255);
-    std::cout << brainLuck(",.,[.",tw) << '\n'; // "cc"
+    tw.append(1, (char)255);
+    std::cout << brainLuck(",.,[.", tw) << '\n'; // "cc"
 
-    //echo until "255";
-    tw = "codewars";
-    tw.append(1,(char)255);
-    std::cout << brainLuck(",+[-.,+]",tw) << '\n'; // "codewars"
+    // //echo until "255";
+    // tw = "codewars";
+    // tw.append(1, (char)255);
+    // std::cout << brainLuck(",+[-.,+]", tw) << '\n'; // "codewars"
 
     //echo until "0";
     // std::string mw = "codewars";
-    // mw.append(1,(char)0);
-    // std::cout << brainLuck(",[.[-],]",mw) << '\n'; // "codewars"
-       
+    // mw.append(1, (char)0);
+    // // endless recursion
+    // std::cout << brainLuck(",[.[-],]", mw) << '\n'; // "codewars"
+
     //two number multiplier
     // std::string dw;
     // dw.append(1, (char) 7);
     // dw.append(1, (char) 3);
     // std::string result_string;
     // result_string.append(1, (char)21);
-    // std::cout << brainLuck(",>,<[>[->+>+<<]>>[-<<+>>]<<<-]>>.",dw << '\n'; // 
+    // std::cout << brainLuck(",>,<[>[->+>+<<]>>[-<<+>>]<<<-]>>.",dw << '\n'; //
 
     return 0;
 }
