2b98bcafc6519c37b335d3cbd3c4bd4acf8a03c9
SimeonHristov99 <s.e.hristov99@gmail.com>
2021-11-05 16:42:17 +0200

    modification

--- task13/task13.py
+++ task13/task13.py
@@ -1,49 +1,19 @@
+from string import ascii_lowercase as abc, ascii_uppercase as ABC
 from math import ceil
 
+def _code(string, shift, mode):
+    return ''.join(
+        abc[(abc.index(c) + i*mode + shift) % len(abc)] if c in abc else
+        ABC[(ABC.index(c) + i*mode + shift) % len(ABC)] if c in ABC else c
+        for i, c in enumerate(string))
 
-def moving_shift(s, shift):
-    len_list = ceil(len(s) / 5)
-    current_len = 0
-    result = []
+def moving_shift(string, shift):
+    encoded = _code(string, shift, 1)
+    cut = int(ceil(len(encoded) / 5.0))
+    return [encoded[i : i+cut] for i in range(0, 5 * cut, cut)]
 
-    current_s = ''
-    for c in s:
-        if c.isalpha():
-            is_up = c.isupper()
-            if is_up:
-                current_s += chr(ord('A') + ((ord(c) - ord('A') + shift) % 26))
-            else:
-                current_s += chr(ord('a') + ((ord(c) - ord('a') + shift) % 26))
-        else:
-            current_s += c
-        shift += 1
-
-    while len(current_s) > 0:
-        result.append(current_s[:len_list])
-        current_s = current_s[len_list:]
-
-    while len(result) < 5:
-        result.append('')
-
-    return result
-
-
-def demoving_shift(s, shift):
-    s = ''.join(s)
-    result = ''
-
-    for c in s:
-        if c.isalpha():
-            is_up = c.isupper()
-            if is_up:
-                result += chr(ord('A') + ((ord(c) - ord('A') - shift) % 26))
-            else:
-                result += chr(ord('a') + ((ord(c) - ord('a') - shift) % 26))
-        else:
-            result += c
-        shift += 1
-
-    return result
+def demoving_shift(arr, shift):
+    return _code(''.join(arr), -shift, -1)
 
 
 def main():
