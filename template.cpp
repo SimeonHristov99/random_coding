8690fac0b666f6aad113d65a20e488f035d400ac
SimeonHristov99 <s.e.hristov99@gmail.com>
2022-04-11 14:13:18 +0300

    Remove boiler plate from test file.

--- template.cpp
+++ template.cpp
@@ -1,31 +1,7 @@
-#define DOCTEST_CONFIG_IMPLEMENT
-#define DOCTEST_VALUE_PARAMETERIZED_DATA
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "../doctest.h"
-
-#include <iostream>
 
 TEST_CASE("Test case name")
 {
     CHECK_EQ(5, 5);
-}
-
-int main(int argc, char **argv)
-{
-    doctest::Context context;
-
-    context.setOption("abort-after", 5);   // stop test execution after 5 failed assertions
-    context.setOption("order-by", "name"); // sort the test cases by their name
-
-    context.applyCommandLine(argc, argv);
-
-    int res = context.run(); // run the tests
-
-    if (context.shouldExit()) // query flags (and --exit) rely on the user doing this
-    {
-        return res; // propagate the result of the tests
-    }
-
-    int client_stuff_return_code = 0;
-
-    return res + client_stuff_return_code; // the result from doctest is propagated here as well
 }
